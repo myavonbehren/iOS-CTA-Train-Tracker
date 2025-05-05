cb3aba98966b6988d0672f130a333027b7ff77b8
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-05 09:54:50 -0500

    Update root view controller to table view

--- mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
+++ mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
@@ -28,5 +28,9 @@ class ViewController: UIViewController, UISearchBarDelegate {
         print("Searching for: \(searchText)")
     }
     
+    /*
+     https://data.cityofchicago.org/resource/8pix-ypme.json
+     */
+    
 }
 
