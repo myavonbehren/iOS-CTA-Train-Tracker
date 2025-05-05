976dece0cda31f69c9a19af5e88b552651658ae7
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-05 11:58:55 -0500

    Add comments

--- mvonbehr-hw2/mvonbehr-hw2/Station.swift
+++ mvonbehr-hw2/mvonbehr-hw2/Station.swift
@@ -7,16 +7,17 @@
 
 import Foundation
 
-
+// Make custom data types compatible with JSON
 struct Station: Codable {
     var stationName: String
     var mapID: String
     var isRed: Bool
     
+    // Map between Swift property and JSON field name
     enum CodingKeys: String, CodingKey {
         case stationName = "station_name"
         case mapID = "map_id"
-        case isRed
+        case isRed = "red"
     }
     
 }
