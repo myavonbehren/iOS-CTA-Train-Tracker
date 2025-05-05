90aae560566941f2827e146326fd753210468ac3
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-05 00:55:07 -0500

    Update UI, change dropdown menu to seperate buttons

--- mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
+++ mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var popUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        popUpButton.showsMenuAsPrimaryAction = true
+        setupMenuActions()
+    }
+    
+    func setupMenuActions() {
+        guard let menu = popUpButton.menu else { return }
+        debugPrint(menu)
     }
 
-
+    
 }
 
