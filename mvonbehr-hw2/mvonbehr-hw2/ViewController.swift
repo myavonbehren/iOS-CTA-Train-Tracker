fdba6c2d104400a04582492565f5eecc30fdbfc8
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-05 11:08:20 -0500

    Update ui

--- mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
+++ mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
@@ -9,20 +9,13 @@ import UIKit
 
 class ViewController: UIViewController, UISearchBarDelegate {
     
-    @IBOutlet var trainLines: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func trainLineSelected(_ sender: UIButton) {
-        for button in trainLines {
-            button.isSelected = false
-        }
-    
-        sender.isSelected = true
-    }
+   
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print("Searching for: \(searchText)")
