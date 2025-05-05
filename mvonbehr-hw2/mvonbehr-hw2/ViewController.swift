a7dab7dbf50b310b2004a6dfd48017c34ae8f20c
Mya Von Behren <mvonbehr@depaul.edu>
2025-05-04 23:07:01 -0500

    Create project

---
+++ mvonbehr-hw2/mvonbehr-hw2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvonbehr-hw2
+//
+//  Created by Mya Von Behren on 5/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mvonbehr-hw2/mvonbehr-hw2Tests/mvonbehr_hw2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  mvonbehr_hw2Tests.swift
+//  mvonbehr-hw2Tests
+//
+//  Created by Mya Von Behren on 5/4/25.
+//
+
+import Testing
+@testable import mvonbehr_hw2
+
+struct mvonbehr_hw2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
